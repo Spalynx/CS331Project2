36925d96c712b691e1404dc850cd0e5f1d6afc0d
Spalynx <Spalynx@gmail.com>
2016-03-22 23:35:42 -0500

    Implemented most of the classes needed to complete the second half of Chapter 5, Question 8.
    Copypasted the code from 4.11 completed forever ago.
    Implemented 5.12, aka: PizzaOrder class.
    Implemented a super class of PizzaOrder: C5Question8.java.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
-/**
- * Created by Spalynx on 3/22/2016.
+/** CS331Project2
+ * Created by Mark Eikel on 3/22/2016.
+ * @author Mark Eikel
+ * @since 22 Mar. 2016
  */
 public class Main {
     public static void main(String [] args){
         System.out.println("Hello World!");
-        System.out.print("Does this work");
+        System.out.println("Does this work");
 
+        //TODO: implement the main method actions in C5Q8.
+        //TODO: implement the main method actions in C5Q4.
     }
 }
---
+++ src/PizzaOrder.java
@@ -0,0 +1,62 @@
+/** Pizza order pretty much emulates buying <= 3 pizzas at once.
+ * @todo debug and actually document this slop.
+ *
+ * @author Spalynx
+ * @since 22 Mar. 2016
+ */
+public class PizzaOrder {
+    //The pizzas and number of pizzas.
+    private Pizza[] pizzas;
+    protected int num_pizzas;
+
+    //Constructors
+    public PizzaOrder() {
+        setNumPizzas(3);
+        num_pizzas = 3;  }
+    public PizzaOrder(int nP){
+        setNumPizzas(nP);
+    }
+
+    //Accessor for pizzas
+    public Pizza[] getPizzas(){
+        return pizzas;          }
+
+    //Sets the amount of pizzas in the class.
+    public void setNumPizzas(int numPizzas){
+        if(numPizzas > 3 && numPizzas < 1){
+            System.out.println("Amt of pizzas not correct! Try again!");
+        }
+        else {
+            pizzas      = new Pizza[numPizzas];
+            num_pizzas  = numPizzas;
+        }
+    }
+
+    //Outputs the descriptions of each pizza.
+    public String toString(){
+        String description = "";
+        for(Pizza p : pizzas){
+            description += p.getDescription();
+        }
+        return description;
+    }
+
+    //Mutators for the pizzas in this class.
+    public void setPizza1(Pizza pizza1){
+        if (num_pizzas >= 1) pizzas[0] = pizza1;
+    }
+    public void setPizza2(Pizza pizza2){
+        if (num_pizzas >= 2) pizzas[1] = pizza2;
+    }
+    public void setPizza3(Pizza pizza3){
+        if (num_pizzas >= 3) pizzas[2] = pizza3;
+    }
+
+    //Calculates the total cost of these pizzas.
+    public double calcTotal(){
+        return pizzas[0].calcCost()
+                + pizzas[1].calcCost()
+                + pizzas[2].calcCost();
+    }
+
+}
